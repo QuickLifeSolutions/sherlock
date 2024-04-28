0fd15fc6cab9f6385e1da79d875cef21cea0ca0a
Adam Kliment <adam@netmilk.net>
2024-04-28 20:43:00 +0200

    Build & Run passing on Apify

---
+++ actor.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+WORKDIR="/opt/sherlock/"
+INPUT=`apify actor:get-input | jq -r .usernames[] | xargs echo`
+echo "INPUT: $INPUT"
+
+cd $WORKDIR
+
+python3 sherlock/sherlock.py $INPUT
+
+for username in $INPUT; do
+  echo "pushing results for username: $username"
+  sed '$d' $username.txt | jo -a | jo $username:=- | apify actor:push-data
+done
+
+
